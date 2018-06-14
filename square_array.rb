e7795c5d328d8ab73c36f601f92878f433299e64
AyanaZaire <ayanazaire@gmail.com>
2018-06-14 03:29:11 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,12 @@
 def square_array(array)
-  # your code here
+  squared = []
+  array.each { |element| squared << element ** 2 }
+  squared
 end
+
+#def square_array(array)
+  #squared = []
+  #array.each { Math.sqrt(element) }
+  #squared
+#end
+
